edadebb6bbb475c96db6262f469c211bb4d2f93d
SACHIN-SANATANI <159048376+SachinxSanatani@users.noreply.github.com>
2024-02-27 21:56:35 +0530

    Update unbanall.py

--- SACHINxSANATANIxMUSIC/plugins/admins/unbanall.py
+++ SACHINxSANATANIxMUSIC/plugins/admins/unbanall.py
@@ -4,7 +4,7 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from SACHINxSANATANIxMUSIC.utils.daxx_ban import admin_filter
 
-BOT_ID = "6974730627"
+BOT_ID = "6754241147"
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):
