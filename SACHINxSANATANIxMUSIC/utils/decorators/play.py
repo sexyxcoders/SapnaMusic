e8aabf3c6a335f31ac74886328c7778596a7d008
SACHIN-SANATANI <159048376+SachinxSanatani@users.noreply.github.com>
2024-02-28 20:38:09 +0530

    Update play.py

--- SACHINxSANATANIxMUSIC/utils/decorators/play.py
+++ SACHINxSANATANIxMUSIC/utils/decorators/play.py
@@ -20,7 +20,7 @@ from SACHINxSANATANIxMUSIC.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from DAXXMUSIC.utils.inline import botplaylist_markup
+from SACHINxSANATANIxMUSIC.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
