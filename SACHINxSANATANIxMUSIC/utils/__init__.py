1653b64348874f458fe405a22b6620254e35254b
SACHIN-SANATANI <159048376+SachinxSanatani@users.noreply.github.com>
2024-02-28 17:55:05 +0530

    Update __init__.py

--- SACHINxSANATANIxMUSIC/utils/__init__.py
+++ SACHINxSANATANIxMUSIC/utils/__init__.py
@@ -1,28 +1,9 @@
-from SACHINxSANATANIxMUSIC.core.bot import DAXX
-from SACHINxSANATANIxMUSIC.core.dir import dirr
-from SACHINxSANATANIxMUSIC.core.git import git
-from SACHINxSANATANIxMUSIC.core.userbot import Userbot
-from SACHINxSANATANIxMUSIC.misc import dbb, heroku
-
-from SafoneAPI import SafoneAPI
-from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-
-app = DAXX()
-api = SafoneAPI()
-userbot = Userbot()
-
-
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .extraction import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
+from .imposterdb import *
