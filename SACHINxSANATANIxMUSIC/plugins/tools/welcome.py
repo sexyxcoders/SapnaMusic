7a9d92f2c60239c904e155897d44b29e18f08853
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-03-11 14:30:38 +0530

    Update welcome.py

--- SACHINxSANATANIxMUSIC/plugins/tools/welcome.py
+++ SACHINxSANATANIxMUSIC/plugins/tools/welcome.py
@@ -187,8 +187,8 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             welcomeimg = welcomepic(
                 pic, user.first_name, member.chat.title, user.id, user.username
             )
-            button_text = "⊙ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ⊙"
-            add_button_text = "✙ ᴋɪᴅɴᴀᴘ ᴍᴇ ✙"
+            button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
+            add_button_text = "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏"
             deep_link = f"tg://openmessage?user_id={user.id}"
             add_link = f"https://t.me/{app.username}?startgroup=true"
             temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
