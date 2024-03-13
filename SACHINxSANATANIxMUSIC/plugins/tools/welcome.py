31bce0594ec9bf444674e26b4d5f04df1ccdc026
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-03-13 16:46:09 +0530

    Update welcome.py

--- SACHINxSANATANIxMUSIC/plugins/tools/welcome.py
+++ SACHINxSANATANIxMUSIC/plugins/tools/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from SACHINxSANATANIxMUSIC.utils.Shukla_ban import admin_filter
+from SACHINxSANATANIxMUSIC.utils.daxx_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
