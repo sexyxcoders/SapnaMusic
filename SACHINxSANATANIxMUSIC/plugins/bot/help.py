051b068d65f1757acf4a62aaa11cbb8bbfcadb6b
SACHIN SANATANI <160611013+SACHIN-SANATANI@users.noreply.github.com>
2024-03-26 22:43:28 +0530

    Update help.py

--- SACHINxSANATANIxMUSIC/plugins/bot/help.py
+++ SACHINxSANATANIxMUSIC/plugins/bot/help.py
@@ -11,7 +11,7 @@ from strings import get_string, helpers
 from SACHINxSANATANIxMUSIC.utils.stuffs.buttons import BUTTONS
 from SACHINxSANATANIxMUSIC.utils.stuffs.helper import Helper
 
-@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["IShelp"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
