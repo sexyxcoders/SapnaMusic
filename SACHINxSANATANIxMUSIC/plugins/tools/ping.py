333ecf393eff72e816842e395681ad8b9f268567
SACHIN SANATANI <160611013+SACHIN-SANATANI@users.noreply.github.com>
2024-03-26 22:38:32 +0530

    Update ping.py

--- SACHINxSANATANIxMUSIC/plugins/tools/ping.py
+++ SACHINxSANATANIxMUSIC/plugins/tools/ping.py
@@ -32,7 +32,7 @@ async def make_carbon(code):
     output_image.name = "carbon.png"
     return output_image
 
-@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ISping", prefixes=["/"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     PING_IMG_URL = "https://telegra.ph/file/7bb907999ea7156227283.jpg"
