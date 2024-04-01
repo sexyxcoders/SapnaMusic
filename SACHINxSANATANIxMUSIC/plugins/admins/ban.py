87c62bddb95104dc37f848511e22db86cfca46b1
It's RAJPUT <160611013+Its-my-edit@users.noreply.github.com>
2024-04-01 18:50:57 +0530

    Update ban.py

--- SACHINxSANATANIxMUSIC/plugins/admins/ban.py
+++ SACHINxSANATANIxMUSIC/plugins/admins/ban.py
@@ -149,7 +149,7 @@ async def unmute_user(user_id, first_name, admin_id, admin_name, chat_id):
     
 
 
-@app.on_message(filters.command(["ban"]))
+@app.on_message(filters.command(["ISban"]))
 async def ban_command_handler(client, message):
     chat = message.chat
     chat_id = chat.id
@@ -203,7 +203,7 @@ async def ban_command_handler(client, message):
         await message.reply_text(msg_text)
 
 
-@app.on_message(filters.command(["unban"]))
+@app.on_message(filters.command(["ISunban"]))
 async def unban_command_handler(client, message):
     chat = message.chat
     chat_id = chat.id
@@ -245,7 +245,7 @@ async def unban_command_handler(client, message):
 
 
 
-@app.on_message(filters.command(["mute"]))
+@app.on_message(filters.command(["ISmute"]))
 async def mute_command_handler(client, message):
     chat = message.chat
     chat_id = chat.id
@@ -300,7 +300,7 @@ async def mute_command_handler(client, message):
         await message.reply_text(msg_text)
 
 
-@app.on_message(filters.command(["unmute"]))
+@app.on_message(filters.command(["ISunmute"]))
 async def unmute_command_handler(client, message):
     chat = message.chat
     chat_id = chat.id
@@ -343,7 +343,7 @@ async def unmute_command_handler(client, message):
 
 
 
-@app.on_message(filters.command(["tmute"]))
+@app.on_message(filters.command(["IStmute"]))
 async def tmute_command_handler(client, message):
     chat = message.chat
     chat_id = chat.id
