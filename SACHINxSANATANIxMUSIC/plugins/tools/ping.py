9f9fa3fd4d568c1fc8226ffd1b3ffa60b01143db
It's RAJPUT <160611013+Its-my-edit@users.noreply.github.com>
2024-04-09 17:54:39 +0530

    Update ping.py

--- SACHINxSANATANIxMUSIC/plugins/tools/ping.py
+++ SACHINxSANATANIxMUSIC/plugins/tools/ping.py
@@ -35,7 +35,7 @@ async def make_carbon(code):
 @app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    PING_IMG_URL = "https://telegra.ph/file/7bb907999ea7156227283.jpg"
+    PING_IMG_URL = "https://telegra.ph/file/ff859741222c7486f79c0.jpg"
     captionss = "**ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ.**"
     response = await message.reply_photo(PING_IMG_URL, caption=(captionss))
     await asyncio.sleep(1)
