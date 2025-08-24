b9e182dc2ea9769d2ee668ad1577d81c0bf3341b
EgoBots <Shizubots@outlook.com>
2025-08-24 18:29:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -144,7 +144,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://ibb.co/xtbmK9dy"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
+    "PING_IMG_URL", "https://ibb.co/xtbmK9dy"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
