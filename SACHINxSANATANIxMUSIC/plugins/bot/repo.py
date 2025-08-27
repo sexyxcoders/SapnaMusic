75ef3818b7a75a1842bb0ec4077fc5184efb2de2
EgoBots <Shizubots@outlook.com>
2025-08-27 12:47:29 +0530

    Update repo.py

--- SACHINxSANATANIxMUSIC/plugins/bot/repo.py
+++ SACHINxSANATANIxMUSIC/plugins/bot/repo.py
@@ -2,51 +2,35 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from SACHINxSANATANIxMUSIC import app
 from config import BOT_USERNAME
-from SACHINxSANATANIxMUSIC.utils.errors import capture_err
-import httpx 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-start_txt = '''
+start_txt = """
 **:⧽ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛᴇᴧᴍ ˹ᴧᴧʀᴜᴍɪ ꭙ ʙᴏᴛѕ˼ ʀᴇᴘᴏs**
- 
+
 **:⧽ ʀᴇᴘᴏ ᴛᴏ ɴʜɪ ᴍɪʟᴇɢᴧ ʙʜᴧɪ ʏʜᴧ**
- 
+
 **:⧽ ᴧɢᴧʀ ʀᴇᴘᴏ ᴄʜᴧʜɪʏᴇ ᴛᴏ ʙʜᴧɪ ʀᴇᴘᴏ ᴘᴧɪᴅ ᴍɪʟ ᴊᴀʏᴇɢᴧ**
- 
+
 **:⧽ ᴄᴏɴᴛᴧᴄᴛ :    - к ᴧ ʀ м ᴧ › ᴏᴘ ⇢**
 
 **:⧽ ʀᴜɴ 24x7 ʟᴧɢ ϝʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ**
- 
-'''
-
-
-
+"""
 
 @app.on_message(filters.command("repo"))
 async def start(_, msg):
     buttons = [
-        [ 
-          InlineKeyboardButton("⊚ ᴧᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴧᴛ ⊚", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
-        ],
-        [
-          InlineKeyboardButton("ᴜᴘᴅᴧᴛᴇ", url="https://t.me/AarumiBots"),
-          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AarumiChat"),
-          ],
-               [
-                InlineKeyboardButton("ʙᴏᴛꜱ ᴏᴡɴᴇʀ", url=f"https://t.me/Swagger_Soul"),
-],
-[
-InlineKeyboardButton("ᴏꜰꜰɪᴄɪᴧʟ ᴍυꜱɪᴄ ʙᴏᴛ", url=f"https://t.me/AarumiSongBot"),
+        [InlineKeyboardButton("⊚ ᴧᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴧᴛ ⊚", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [InlineKeyboardButton("ᴜᴘᴅᴧᴛᴇ", url="https://t.me/AarumiBots"),
+         InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AarumiChat")],
+        [InlineKeyboardButton("ʙᴏᴛꜱ ᴏᴡɴᴇʀ", url="https://t.me/Swagger_Soul")],
+        [InlineKeyboardButton("ᴏꜰꜰɪᴄɪᴧʟ ᴍυꜱɪᴄ ʙᴏᴛ", url="https://t.me/AarumiSongBot")]
+    ]
 
-        ]]
-    
     reply_markup = InlineKeyboardMarkup(buttons)
-    
+
     await msg.reply_photo(
         photo="https://telegra.ph/file/9131bdd30ab9c76349f25.jpg",
         caption=start_txt,
         reply_markup=reply_markup,
-        has_spoiler=True,
-        parse_mode="html"
- 
+        parse_mode="markdown",
+        has_spoiler=True
     )
