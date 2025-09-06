a9f9a51e6b2380e34577d7a4368438fcf21067ab
Ego80 <Shizubots@outlook.com>
2025-09-06 21:04:38 +0530

    Update repo.py

--- SACHINxSANATANIxMUSIC/plugins/bot/repo.py
+++ SACHINxSANATANIxMUSIC/plugins/bot/repo.py
@@ -1,42 +1,47 @@
-from pyrogram import Client, filters
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from SACHINxSANATANIxMUSIC import app
+from config import BOT_USERNAME
+from SACHINxSANATANIxMUSIC.utils.errors import capture_err
+import httpx 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Import your bot app and username from your main bot file
-from SACHINxSANATANIxMUSIC import app
-from config import BOT_USERNAME 
-
 start_txt = """
-**:⧽ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛᴇᴧᴍ ˹ᴧᴧʀᴜᴍɪ ꭙ ʙᴏᴛѕ˼ ʀᴇᴘᴏs**
-
-**:⧽ ʀᴇᴘᴏ ᴛᴏ ɴʜɪ ᴍɪʟᴇɢᴧ ʙʜᴧɪ ʏʜᴧ**
-
-**:⧽ ᴧɢᴧʀ ʀᴇᴘᴏ ᴄʜᴧʜɪʏᴇ ᴛᴏ ʙʜᴧɪ ʀᴇᴘᴏ ᴘᴧɪᴅ ᴍɪʟ ᴊᴀʏᴇɢᴧ**
-
-**:⧽ ᴄᴏɴᴛᴧᴄᴛ : - к ᴧ ʀ м ᴧ › ᴏᴘ ⇢**
-
-**:⧽ ʀᴜɴ 24x7 ʟᴧɢ ϝʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ**
+〄 ᴡᴇʟᴄᴏᴍᴇ ғᴏʀ sᴀᴄʜɪɴ sᴀɴᴀᴛᴀɴɪ ʀᴇᴘᴏs 〄
+ 
+࿃ ᴍᴇʀᴀ ʟᴀɴᴅ ʟᴇ ʟᴇ
+ 
+࿃ ᴛᴇʀɪ ᴍᴀ ᴋɪ ᴄʜᴜᴛ ʀᴀɴᴅɪ ᴋᴇ
+ 
+࿃ ʀᴜɴ 24x7 ʟᴀɢ ғʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ
+ 
 """
 
-@app.on_message(filters.command("repo") & (filters.private | filters.group))
-async def repo_handler(_, msg):
+
+
+
+@app.on_message(filters.command("ISrepo"))
+async def start(_, msg):
     buttons = [
-        [InlineKeyboardButton("⊚ ᴧᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴧᴛ ⊚", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [
-            InlineKeyboardButton("ᴜᴘᴅᴧᴛᴇ", url="https://t.me/AarumiBots"),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AarumiChat")
+        [ 
+          InlineKeyboardButton("ＡＤＤ ＭＥ ＢＡＢＹ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
-        [InlineKeyboardButton("ʙᴏᴛꜱ ᴏᴡɴᴇʀ", url="https://t.me/Swagger_Soul")],
-        [InlineKeyboardButton("ᴏꜰꜰɪᴄɪᴛʟ ᴍυꜱɪᴄ ʙᴏᴛ", url="https://t.me/AarumiSongBot")]
-    ]
+        [
+          InlineKeyboardButton("ＨＥＬＰ", url="https://t.me/SANATANI_IS_HERE"),
+          InlineKeyboardButton("ＳＡＣＨＩＮ", url="https://t.me/V_VIP_OWNER"),
+          ],
+               [
+                InlineKeyboardButton("ＳＡＮＡＴＡＮＩ ＮＥＴＷＯＲＫ", url=f"https://t.me/SANATANI_IS_HERE"),
+],
+[
+InlineKeyboardButton("ＯＦＦＩＣＩＡＬ ＢＯＴ", url=f"https://t.me/HIMANSHI_MUSIC_BOT"),
 
+        ]]
+    
     reply_markup = InlineKeyboardMarkup(buttons)
-
+    
     await msg.reply_photo(
         photo="https://telegra.ph/file/9131bdd30ab9c76349f25.jpg",
         caption=start_txt,
-        reply_markup=reply_markup,
-        parse_mode="HTML",  
-        disable_notification=True,
-        has_spoiler=True
+        reply_markup=reply_markup
     )
-    
