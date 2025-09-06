081c4a87cea0fc6dbcc31f021b236aa1efbfb20a
Ego80 <Shizubots@outlook.com>
2025-09-06 20:11:44 +0530

    Update repo.py

--- SACHINxSANATANIxMUSIC/plugins/bot/repo.py
+++ SACHINxSANATANIxMUSIC/plugins/bot/repo.py
@@ -1,7 +1,9 @@
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# Import your bot app and username from your main bot file
 from SACHINxSANATANIxMUSIC import app
-from config import BOT_USERNAME
+from config import BOT_USERNAME 
 
 start_txt = """
 **:⧽ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛᴇᴧᴍ ˹ᴧᴧʀᴜᴍɪ ꭙ ʙᴏᴛѕ˼ ʀᴇᴘᴏs**
@@ -10,19 +12,21 @@ start_txt = """
 
 **:⧽ ᴧɢᴧʀ ʀᴇᴘᴏ ᴄʜᴧʜɪʏᴇ ᴛᴏ ʙʜᴧɪ ʀᴇᴘᴏ ᴘᴧɪᴅ ᴍɪʟ ᴊᴀʏᴇɢᴧ**
 
-**:⧽ ᴄᴏɴᴛᴧᴄᴛ :    - к ᴧ ʀ м ᴧ › ᴏᴘ ⇢**
+**:⧽ ᴄᴏɴᴛᴧᴄᴛ : - к ᴧ ʀ м ᴧ › ᴏᴘ ⇢**
 
 **:⧽ ʀᴜɴ 24x7 ʟᴧɢ ϝʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ**
 """
 
-@app.on_message(filters.command("repo"))
-async def start(_, msg):
+@app.on_message(filters.command("repo") & (filters.private | filters.group))
+async def repo_handler(_, msg):
     buttons = [
         [InlineKeyboardButton("⊚ ᴧᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴧᴛ ⊚", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [InlineKeyboardButton("ᴜᴘᴅᴧᴛᴇ", url="https://t.me/AarumiBots"),
-         InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AarumiChat")],
+        [
+            InlineKeyboardButton("ᴜᴘᴅᴧᴛᴇ", url="https://t.me/AarumiBots"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AarumiChat")
+        ],
         [InlineKeyboardButton("ʙᴏᴛꜱ ᴏᴡɴᴇʀ", url="https://t.me/Swagger_Soul")],
-        [InlineKeyboardButton("ᴏꜰꜰɪᴄɪᴧʟ ᴍυꜱɪᴄ ʙᴏᴛ", url="https://t.me/AarumiSongBot")]
+        [InlineKeyboardButton("ᴏꜰꜰɪᴄɪᴛʟ ᴍυꜱɪᴄ ʙᴏᴛ", url="https://t.me/AarumiSongBot")]
     ]
 
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -31,6 +35,8 @@ async def start(_, msg):
         photo="https://telegra.ph/file/9131bdd30ab9c76349f25.jpg",
         caption=start_txt,
         reply_markup=reply_markup,
-        parse_mode="markdown",
+        parse_mode="markdown",  
+        disable_notification=True,
         has_spoiler=True
     )
+    
